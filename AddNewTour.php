90907ea108c690e9d2a27fe0b5883f75b3f29366
jiwonlee <jiwonlee0120@gmail.com>
2022-08-27 00:44:08 -0700

    AddNewTour is modified

--- AddNewTour.php
+++ AddNewTour.php
@@ -24,21 +24,34 @@
     </section>
     <section class="right">
         <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
-            <label for="cName">Destination</label>
-            <input name="cName" required/>
-            <label for="cName">Duration</label>
-            <input name="length" type="number" max="127" required />
-            <label for="cName">Destination Img</label>
-            <input name="cImg" type="file" />
+            <label for="category">Category</label>
+            <select name="category">
+                <option>GUIDED TOURS</option>
+                <option>ATTRACTIONS + ACTIVITIES</option>
+                <option>FEATURED - PRODUCTS</option>
+            </select>
+            <label for="des_name">Destination</label>
+            <input name="des_name" required/>
+            <label for="des_img">Destination Img</label>
+            <input name="des_img" type="file" />
+            <label for="des_exp">Explanation</label>
+            <input name="des_exp" required/>
+            <label for="duration">Duration</label>
+            <input name="duration" required/>
+            <label for="price">Price</label>
+            <input name="price" required/>
             <button type="submit">Register</button>
         </form>
         <?php
         if($_SERVER['REQUEST_METHOD']=="POST"){
-            $cName = $_POST['cName'];
-            $length = $_POST['length'];
-            $sourceImg = $_FILES['cImg'];
+            $category = $_POST['category'];
+            $des_name = $_POST['des_name'];
+            $des_exp = $_POST['des_exp'];
+            $duration = $_POST['duration'];
+            $price = $_POST['price'];
+            $sourceImg = $_FILES['des_img'];
             $imgExtension = pathinfo($sourceImg['name'])['extension'];
-            $imgDest = "./files/cimg/".str_replace(" ","_",$cName)."img.".$imgExtension;
+            $imgDest = "./files/des_img/".str_replace(" ","_",$des_name)."img.".$imgExtension;
             if($imgExtension == "jpg" && getimagesize($sourceImg['tmp_name'])){
                 if($sourceImg['size']<400000){
                     if(move_uploaded_file($sourceImg['tmp_name'],$imgDest)){
@@ -46,7 +59,7 @@
                         if($dbcon->connect_error){
                             echo "<h1>".$dbcon->connect_error."</h1>";
                         }else{
-                            $insertCmd = "INSERT INTO course_tb (coursename,length,course_img) VALUES ('$cName',$length,'$imgDest')";
+                            $insertCmd = "INSERT INTO destination_tb (category,des_name,des_img,des_exp,duration,price) VALUES ('$category','$des_name','$imgDest','$des_exp','$duration','$price')";
                             if($dbcon->query($insertCmd)===TRUE){
                                 echo "<h1>New Tour is registered</h1>";
                             }else{
