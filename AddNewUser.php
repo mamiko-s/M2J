ab68a1c17a991f99e2d249ca48b9b6d36814c0f5
jiwonlee <jiwonlee0120@gmail.com>
2022-08-27 00:13:25 -0700

    AddNewUser is modified

--- AddNewUser.php
+++ AddNewUser.php
@@ -33,18 +33,16 @@
             <input name="fname" placeholder="Enter a First Name"/>
             <label for="lname">Last Name:</label> 
             <input name="lname" placeholder="Enter a Last Name"/>
-            <label for="email">Email:</label> 
-            <input type="email" placeholder="Enter a Email"/>
-            <label for="password">Password:</label> 
-            <input type="password" name="password" placeholder="Enter a Password"/>
             <label for="dob">Date of birth:</label>
             <input type="date" name="dob" require/>
+            <label for="email">Email:</label> 
+            <input type="email" name="email" placeholder="Enter a Email"/>
+            <label for="password">Password:</label> 
+            <input type="password" name="pass" placeholder="Enter a Password"/>
             <label for="phone">Phone:</label> 
             <input type="text" name="phone" placeholder="Enter a Phone Number"/>
-            <label for="addr">Address:</label>
-            <input name="addr" require/>
             <label for="UserType">Type of User:</label> 
-            <select name="UserType">
+            <select name="usertype">
                 <option value="admin">Admin</option>
                 <option value="user">User</option>
             </select>
@@ -57,7 +55,7 @@
                     die ("Connection error ".$dbCon->connect_error);
                 }else{
                     $dbPass = password_hash($_POST['pass'],PASSWORD_BCRYPT,["cost"=>9]);
-                    $insertCmd = "INSERT INTO user_tb (firstName,lastName,email,pass,dob,phone,addr) VALUES ('".$_POST['fname']."','".$_POST['lname']."','".$_POST['email']."','".$dbPass."','".$_POST['dob']."','".$_POST['phone']."','".$_POST['addr']."')";
+                    $insertCmd = "INSERT INTO user_tb (firstName,lastName,dob,email,pass,phone,usertype) VALUES ('".$_POST['fname']."','".$_POST['lname']."','".$_POST['dob']."','".$_POST['email']."','".$dbPass."','".$_POST['phone']."','".$_POST['usertype']."')";
                     $result = $dbCon->query($insertCmd);
                     if($result === true){
                         echo "<h1 style='color: green;'>DONE!!!</h1>";
--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     $dbUsername = "root";
     $dbServername = "localhost";
     $dbPass = "";
-    $dbname = "students_db";
+    $dbname = "user_db";
     
     function createCookie($cookieName, $cookieValue, $expDay = 1){
         setcookie($cookieName,$cookieValue,time()+(86400 * $expDay),'/');//86400 is equal to 1 day = 24(hours) * 60(minutes) * 60(seconds)
