e3caebb841e10a9dcaf407fcfcb0af4c49acc553
mamiko-s <mamiko.sugai@gmail.com>
2022-08-27 22:59:09 -0700

    adminHome html/css modified

--- adminHome.php
+++ adminHome.php
@@ -21,34 +21,40 @@
     <title>Document</title>
     <link rel="stylesheet" href="./css/adminHome.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <main>
-        <section class="left">
-            <h2>UserName</h2>
-            <i class="fa-solid fa-user"></i>
+        <section class="nav_section">
+            <div class="title_top">
+                <h2>M2J Trip</h2>
+                <span><i class="fas fa-user-circle"></i></span>
+                <p>User Name</p>
+            </div>
             <ul>
-                <li><i class="fa-solid fa-house-user"></i><a href="#">ADMIN HOME</a></li>
-                <li><i class="fa-solid fa-user"></i><a href="#">ADD NEW USER</a></li>
-                <li><i class="fa-solid fa-user-pen"></i><a href="#">EDIT USER</a></li>
-                <li><i class="fa-solid fa-map-location-dot"></i><a href="#">ADD NEW TOUR</a></li>
+                <li class="link_hover"><i class="fas fa-home"></i><a href="adminHome.php">ADMIN HOME</a></li>
+                <li><i class="fas fa-user-plus"></i><a href="AddNewUser.php">ADD NEW USER</a></li>
+                <li><i class="fas fa-edit"></i><a href="EditUser.php">EDIT USER</a></li>
+                <li><i class="fa-solid fa-map-location-dot"></i><a href="AddNewTour.php">ADD NEW TOUR</a></li>
             </ul>
-            <a href="#">LOGOUT</a>
+            <button type="submit">LOGOUT</button>
         </section>
-        <section class="right">
+
+        <section class="panel_section">
             <h1>Admin Dashboard</h1>
+            <h3>User Information</h3>
             <article>
-                <h2>USER INFO</h2>
-                <table border="1">
+                <table>
                 <thead>
                     <tr>
-                        <th>User ID</th>
-                        <th>Full name</th>
-                        <th>email</th>
-                        <th>Date of birth</th>
-                        <th>Phone</th>
-                        <th>User Type</th>
-
+                        <th width="10%">User ID</th>
+                        <th width="20%">Full name</th>
+                        <th width="23%">email</th>
+                        <th width="16%">Date of birth</th>
+                        <th width="16%">Phone</th>
+                        <th width="15%">User Type</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -64,8 +70,8 @@
                                 echo "<tr>";
                                 echo "<td>".$row['user_id']."</td>";
                                 echo "<td>".$row['firstName']." ".$row['lastName']."</td>";
-                                echo "<td>".$row['dob']."</td>";
                                 echo "<td>".$row['email']."</td>";
+                                echo "<td>".$row['dob']."</td>";
                                 echo "<td>".$row['phone']."</td>";
                                 echo "<td>".$row['usertype']."</td>";
                                 echo "</tr>";
@@ -76,17 +82,18 @@
                 </tbody>
             </table>
             </article>
+            
+            <h3>Destinations</h3>
             <article>
-                <h2>DESTINATION</h2>
-                <table border="1">
+                <table>
                 <thead>
                     <tr>
-                        <th>Destination Id</th>
-                        <th>Destination</th>
-                        <th>Image</th>
-                        <th>Explanation</th>
-                        <th>Duration</th>
-                        <th>Price</th>
+                        <th width="10%">Id</th>
+                        <th width="15%">Destination</th>
+                        <th width="5%">Image</th>
+                        <th width="30%">Explanation</th>
+                        <th width="20%">Duration</th>
+                        <th width="20%">Price</th>
 
                     </tr>
                 </thead>
