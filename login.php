0b28d0e7d51726614738680a6f10beaca55b2cd2
LukeShin3022 <jwshin3022@gmail.com>
2022-08-29 23:03:46 -0700

    making login Function

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+<?php
+    include "./config.php";
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +18,6 @@
 <body id="login">
     <section class="login_page">
         <div class="login_panel">
-            
             <h3>
                 Welcome to M2J Trip!
             </h3>
@@ -30,5 +34,40 @@
             </form>
         </div>
     </section>
+    <?php
+        if($_SERVER['REQUEST_METHOD']=="POST"){
+            $username = $_POST['username'];
+            $pass = $_POST['pass'];
+            $dbconn = new mysqli($dbServername, $dbUsername, $dbPass, $dbname);
+            if($dbconn->connect_error){
+                die("Error: ".$dbconn->connect_error);
+            }else{
+                $selectCmd = "SELECT * FROM user_tb WHERE email='$username';";
+                // $selectCmd = "SELECT * FROM `user_tb` WHERE email='$username' and password='$pass';";
+                $result = $dbconn->query($selectCmd);
+                // print_r($result);
+                if($result->num_rows > 0){
+                    echo "User valid";
+                    $user = $result->fetch_assoc();
+                    $hashedPass = $user['password'];
+                    $pass = password_hash($pass,PASSWORD_BCRYPT,["cost" => 8]);
+                    $hashedPass = password_hash($pass, PASSWORD_BCRYPT, ["cost" => 8]);
+                    if(password_verify($pass,$hashedPass)){
+                        $_SESSION['user'] = $user;
+                        $_SESSION['session_timeout'] = time() + 5;
+                        $_SESSION['session_id'] = "1234";
+                        $dbconn->close();
+                        header("Location: http://localhost/PHP/M2J/M2J-adminHome/adminHome.php");
+                    }else{
+                        echo "User not valid";
+                    }
+                }else{
+                    echo "User not valid";
+                }
+            }
+            $dbconn->close();
+        }
+    ?>
+
 </body>
 </html>
