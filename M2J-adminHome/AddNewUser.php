31ca3e3bcf6b27ad550256d8964cb6330da02f7b
mamiko-s <mamiko.sugai@gmail.com>
2022-08-30 22:33:19 -0700

    update

--- M2J-adminHome/AddNewUser.php
+++ M2J-adminHome/AddNewUser.php
@@ -88,10 +88,11 @@
             if($_SERVER['REQUEST_METHOD'] == "POST"){
                 $dbCon = new mysqli($dbServername, $dbUsername, $dbPass, $dbname);
                 if($dbCon->connect_error){
-                    die ("Connection error ".$dbCon->connect_error);
+                    // die ("Connection error ".$dbCon->connect_error);
+                    echo ("Connection error ".$dbCon->connect_error);
                 }else{
                     $dbPass = password_hash($_POST['pass'],PASSWORD_BCRYPT,["cost"=>9]);
-                    $insertCmd = "INSERT INTO user_tb (firstName,lastName,dob,email,pass,phone,usertype) VALUES ('".$_POST['fname']."','".$_POST['lname']."','".$_POST['dob']."','".$_POST['email']."','".$dbPass."','".$_POST['phone']."','".$_POST['usertype']."')";
+                    $insertCmd = "INSERT INTO user_tb (firstName,lastName,dob,email,pass,phone,usertype,session_ID) VALUES ('".$_POST['fname']."','".$_POST['lname']."','".$_POST['dob']."','".$_POST['email']."','".$dbPass."','".$_POST['phone']."','".$_POST['usertype']."','1')";
                     $result = $dbCon->query($insertCmd);
                     if($result === true){
                         echo "<h1 style='color: green;'>DONE!!!</h1>";
