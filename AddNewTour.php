a0ea9a9a699c959291a9a653d039ec52b752e263
mamiko-s <mamiko.sugai@gmail.com>
2022-09-01 13:15:36 -0700

    update css

--- AddNewTour.php
+++ AddNewTour.php
@@ -18,29 +18,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/common.css">
     <link rel="stylesheet" href="./css/AddNewTour.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@400;700;900&display=swap" rel="stylesheet">
 </head>
 <body>
-<main>
+    <header>
+        <h3>M2J Trip</h3>
+        <div>
+            <p>Welcome back <?php echo $_SESSION['user']['firstName'] ?>!</p>
+            <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+              <button type="submit" name="action" value="logout">LOGOUT</button>
+            </form>
+        </div>
+    </header>
+    <main>
     <section class="nav_section">
             <div class="title_top">
-                <h2>M2J Trip</h2>
                 <span><i class="fas fa-user-circle"></i></span>
-                <p>User Name</p>
+                <p><?php echo $_SESSION['user']['firstName'] ?></p>
             </div>
             <ul>
                 <li><i class="fas fa-home"></i><a href="adminHome.php">ADMIN HOME</a></li>
                 <li><i class="fas fa-user-plus"></i><a href="AddNewUser.php">ADD NEW USER</a></li>
-                <li><i class="fas fa-edit"></i><a href="EditUser.php">EDIT USER</a></li>
+                <li><i class="fas fa-edit"></i><a href="EditAdmin.php">EDIT USER</a></li>
                 <li class="link_hover"><i class="fa-solid fa-map-location-dot"></i><a href="AddNewTour.php">ADD NEW TOUR</a></li>
             </ul>
-            <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                <button type="submit" name="action" value="logout">LOGOUT</button>
-            </form>
     </section>
 
     <section class="panel_section">
--- adminHome.php
+++ adminHome.php
@@ -30,19 +30,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/adminHome.css">
+    <link rel="stylesheet" href="./css/common.css">
+    <link rel="stylesheet" href="./css/AdminHome.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@400;700;900&display=swap" rel="stylesheet">
 </head>
 <body>
+    <header>
+        <h3>M2J Trip</h3>
+        <div>
+            <p>Welcome back <?php echo $_SESSION['user']['firstName'] ?>!</p>
+            <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+              <button type="submit" name="action" value="logout">LOGOUT</button>
+            </form>
+        </div>
+    </header>
     <main>
         <section class="nav_section">
             <div class="title_top">
-                <h2>M2J Trip</h2>
                 <span><i class="fas fa-user-circle"></i></span>
-                <p>User Name</p>
+                <p><?php echo $_SESSION['user']['firstName'] ?></p>
             </div>
             <ul>
                 <li class="link_hover"><i class="fas fa-home"></i><a href="adminHome.php">ADMIN HOME</a></li>
@@ -50,9 +60,6 @@
                 <li><i class="fas fa-edit"></i><a href="EditAdmin.php">EDIT USER</a></li>
                 <li><i class="fa-solid fa-map-location-dot"></i><a href="AddNewTour.php">ADD NEW TOUR</a></li>
             </ul>
-            <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                <button type="submit" name="action" value="logout">LOGOUT</button>
-            </form>
         </section>
 
         <section class="panel_section">
@@ -96,17 +103,17 @@
                 </table>
             </article>
 
-            <h3>Destinations</h3>
+            <h3 class="des-title">Destinations</h3>
             <article>
                 <table>
                     <thead>
                         <tr>
-                            <th width="10%">Id</th>
-                            <th width="15%">Destination</th>
-                            <th width="5%">Image</th>
-                            <th width="30%">Explanation</th>
-                            <th width="20%">Duration</th>
-                            <th width="20%">Price</th>
+                            <th width="7%">Id</th>
+                            <th width="23%">Destination</th>
+                            <th width="7%">Image</th>
+                            <th width="35%">Explanation</th>
+                            <th width="14%">Duration</th>
+                            <th width="14%">Price</th>
 
                         </tr>
                     </thead>
@@ -123,7 +130,7 @@
                                 echo "<tr>";
                                 echo "<td>".$row['des_id']."</td>";
                                 echo "<td>".$row['des_name']."</td>";
-                                echo "<td><img src='".$row['des_img']."'></td>";
+                                echo "<td><img style='margin:12px 14px 10px 0; border-radius:12px; width:150px; height:100px;' src='".$row['des_img']."'></td>";
                                 echo "<td>".$row['des_exp']."</td>";
                                 echo "<td>".$row['duration']."</td>";
                                 echo "<td>".$row['price']."</td>";
